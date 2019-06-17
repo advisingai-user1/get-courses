5ed7d93ce82d65810f7ae02b9930c99e7dea8077
Charles <momin@advisingai.com>
2019-06-17 11:37:00 -0500

    *** empty log message ***

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from mechanize import Browser;
+from mechanize import Browser;  # for making requests
+from bs4 import BeautifulSoup;  # for parsing html
 
 def web_parse(url, catoid, coid_begin, coid_end):
 
