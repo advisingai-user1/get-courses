0ded4cf03354d0b454813255d9c9dee30326f60e
Charles <momin@advisingai.com>
2019-06-17 15:34:29 -0500

    Find last course id

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def web_parse(url, catoid, coid_begin, coid_end):
     courses = []
     coid = coid_begin
     while coid <= coid_end:
-        html = request_course(url+"?catoid="+catoid+"&coid="+coid)
+        html = request_course(url+"?catoid="+str(catoid)+"&coid="+str(coid))
         course = process_request(html)
         if course:
             courses.append(course)
@@ -27,9 +27,8 @@ def web_parse(url, catoid, coid_begin, coid_end):
 """
 def request_course(url):
     bro = Browser();            # again a different one
-    connect_nofafil(bro,) # connect to a course page
+    connect_nofail(bro,) # connect to a course page
 
-    # TODO: Visakh
 
 """
 get the course information from the html page
@@ -37,6 +36,7 @@ get the course information from the html page
 """
 def process_request(html):
     # TODO: beautiful soup probably
+    print("TODO")
     
 
 """
@@ -49,10 +49,10 @@ def connect_nofail(bro, url):
     connected = False
     while not connected:
         try:
-            bro.follow_link(url)
+            bro.open(url)
             connected = True
         except:
-            print("retrying")
+            print(".", end='',flush=True)
 
 ### Main ###
 """
@@ -61,29 +61,34 @@ get any user input and get initial information like the last course id
 """
 def main():
     catoid = 40
-    # page with list of courses
-    smuCatalogURL = "https://catalog.smu.edu/content.php?catoid="+catoid+"&navoid=3146"
+    # page with list of courses: southern methodist university catalog uniform resource locator
+    smu_cat_url = "https://catalog.smu.edu/content.php?catoid="+str(catoid)+"&navoid=3146"
     ### find the last courses id
-    coid_begin = 144004
+    coid_begin = 144004         # hopefuly this stays the same forever
     coid_end = 0;               # will set later
     bro = Browser();
-    connect_nofail(bro, smuCatalogURL)
+    bro.set_handle_robots(False)
+    bro.set_handle_refresh(False)
+    bro.set_cookiejar(None) 
+    connect_nofail(bro, smu_cat_url)
     # find the link to get to the last page of courses
     links = list(bro.links())
-    for i in range(len(links)-1, 0):
+    for i in range(len(links)-1, 0, -1):
         text = links[i].text
-        if(text = "Forward 10"):
+        if(text == "Forward 10"):
             # the link after this one is the link to the last page
-            connect_nofail(bro, links[i+1])
+            connect_nofail(bro, links[i+1].url) # we are going to the last page
+            break
     # now being on the last page of courses, we can start from the back again and find the last course
     links = list(bro.links())
-    for i in range(len(links)-1, 0):
+    for i in range(len(links)-1, 0, -1):
         text = links[i].text
-        if(text = "1"):         # this is the link right after the last course
+        if(text == "1"):         # this is the link right after the last course
             # get the course id of the last course
             text = links[i-1].url # the previous link on the page should be the last course
             url_parts = text.split('=')
-            coid_end = url_parts[-1] # the last element is the coid
+            coid_end = int(url_parts[-1]) # the last element is the coid
+            break
     # we got the course ids, now we leave the rest of the work to web_parse()
     courses = web_parse("https://catalog.smu.edu/preview_course_nopop.php", catoid, coid_begin, coid_end)
     # I guess we just print them out for now
